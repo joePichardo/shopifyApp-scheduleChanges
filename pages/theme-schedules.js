ecea5ffb5a30650b44cf4de4bb1b8c51c8cc4040
joepichardo <joepichar@gmail.com>
2020-06-14 11:07:17 -0700

    fix schedule list state and update state on mount when fetching for theme schedules. page changes depending on route success

--- pages/theme-schedules.js
+++ pages/theme-schedules.js
@@ -18,20 +18,58 @@ const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
 class ThemeSchedules extends React.Component {
   state = {
-    scheduleList: {}
+    scheduleList: []
   };
+
+  componentDidMount() {
+    console.log("mount");
+
+    const fetchURL = `/api/themes/schedules`;
+    const options = {
+      method: 'GET'
+    };
+
+    fetch(fetchURL, options)
+      .then(response => response.json())
+      .then(({themeSchedules}) => {
+        console.log('mount themeSchedules', themeSchedules)
+
+        this.setState({
+          scheduleList: themeSchedules,
+        });
+      })
+      .catch(error => alert(error));
+
+  }
 
   render() {
 
     return (
       <Page>
         <Layout>
-          <EmptyState
-            heading="Schedule changes to start"
-            image={img}
-          >
-            <p>Use theme commands page to schedule new changes to your website.</p>
-          </EmptyState>
+          {_.isEmpty(this.state.scheduleList) ?
+            <EmptyState
+              heading="Schedule changes to start"
+              image={img}
+            >
+              <p>Use theme commands page to schedule new changes to your website. Return here to view your scheduled changes.</p>
+            </EmptyState>
+            :
+            <div>
+              {
+                this.state.scheduleList.map(schedule => {
+                  return (
+                    <div key={schedule.id}>
+                      <div>{schedule.description}</div>
+                      <div>Scheduled For: {schedule.scheduleAt}</div>
+                      <div>Deployed {schedule.deployed ? "Yes" : "No"}</div>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          }
+
         </Layout>
       </Page>
     );
