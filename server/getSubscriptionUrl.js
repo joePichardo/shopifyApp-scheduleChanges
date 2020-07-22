1928119bff3a03a910d5c32efa90d76865cd4a61
joepichardo <joepichar@gmail.com>
2020-07-21 22:21:50 -0700

    add back subscription url with fixed query

--- server/getSubscriptionUrl.js
+++ server/getSubscriptionUrl.js
@@ -5,6 +5,7 @@ const getSubscriptionUrl = async (ctx, accessToken, shop) => {
           name: "All-In-One Plan"
           trialDays: 14
           returnUrl: "${process.env.HOST}"
+          test: true
           lineItems: [
           {
             plan: {
