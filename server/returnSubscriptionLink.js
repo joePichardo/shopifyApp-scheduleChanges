4ab5a79dd27a44bc6b51ff605061e1a23ed36ad5
joepichardo <joepichar@gmail.com>
2020-07-28 20:43:00 -0700

    add signup function when returning subscription link

--- server/returnSubscriptionLink.js
+++ server/returnSubscriptionLink.js
@@ -1,3 +1,8 @@
+const getShopEmail = require('./getShopEmail');
+const {
+  BACKEND_ADDRESS
+} = process.env;
+
 const returnSubscriptionLink = async (ctx, accessToken, shop) => {
   const query = JSON.stringify({
     query: `mutation {
@@ -27,6 +32,26 @@ const returnSubscriptionLink = async (ctx, accessToken, shop) => {
         }
     }`
   });
+
+  const bodyObj = {
+    storeAddress: shop,
+    accessToken: accessToken
+  };
+
+  const responseEmail = await getShopEmail(ctx, accessToken, shop);
+
+  if (responseEmail && responseEmail.data.shop.contactEmail) {
+    bodyObj["email"] = responseEmail.data.shop.contactEmail;
+  }
+
+  const responseSignUp = await fetch(`${BACKEND_ADDRESS}/account/signup`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      "X-Shopify-Access-Token": accessToken,
+    },
+    body: JSON.stringify(bodyObj)
+  })
 
   const response = await fetch(`https://${shop}/admin/api/2019-10/graphql.json`, {
     method: 'POST',
